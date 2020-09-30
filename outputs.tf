c6eb21737d31663bc9e2b9ce9d1bb151ea9b59b0
kgurudut <kgurudut@in.ibm.com>
2020-09-30 20:45:21 +0530

    initial commit

---
+++ outputs.tf
@@ -0,0 +1,27 @@
+output "cluster_id" {
+  value       = "${ibm_container_cluster.cluster.id}"
+  description = "IKS ROKS cluster ID"
+}
+
+output "ingress_hostname" {
+  value       = "${ibm_container_cluster.cluster.ingress_hostname}"
+  description = "ingress subdomain host name of cluster"
+}
+
+output "ingress_secret" {
+  value       = "${ibm_container_cluster.cluster.ingress_secret}"
+  description = "name of ingress subdomain secret"
+}
+
+output "tags" {
+  value       = "${ibm_container_cluster.cluster.tags}"
+  description = "tags for the cluster"
+}
+
+output "worker_num" {
+  value       = "${ibm_container_cluster.cluster.worker_num}"
+  description = "number of workers for the cluster"
+}
+
+
+
